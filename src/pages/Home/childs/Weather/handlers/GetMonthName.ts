91b636f79907c6539aed03c1abe2fac14142a745
Мамбетов Азамат <a.mambetov@lan-project.ru>
2020-08-13 20:28:07 +0300

    Обновил README.

--- src/pages/Home/childs/Weather/handlers/GetMonthName.ts
+++ src/pages/Home/childs/Weather/handlers/GetMonthName.ts
@@ -1,15 +1,17 @@
+import { Language } from "../../../../../store/SearchOptions/SearchOptions.state";
+
 export const getMonthName = (monthIndex: number, lang: string): string => {
 	let monthNames: string[] = null;
 
 	switch(lang) {
-		case "ru":
+		case Language.RU:
 			monthNames = [
 				"Январь", "Февраль", "Март", "Апрель",
 				"Май", "Июнь", "Июль", "Август",
 				"Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
 			];
 			break;
-		case "en":
+		case Language.EN:
 			monthNames = [
 				"January", "February", "March", "April", "May",
 				"June", "July", "August", "September", "October",
